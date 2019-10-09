230007dda400db359e07669f81a60c5d4ed50da4
dreak603 <dreak603@gmail.com>
2019-10-09 17:28:17 +0800

    add mongo prod

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const databaseConnect = () => {
-    return mongoose.connect('mongodb://localhost/shortenUrl', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shortenUrl', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     const db = mongoose.connection
     db.on('error', () => {
         console.log('mongodb error!')
